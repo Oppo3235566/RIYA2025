17199bc30d5b6ffc4b676ad1a2242baca4793e4c
SHAHADAT SAHU <sahushahadat@gmail.com>
2026-01-30 22:16:52 +0600

    Update pick.js

--- Script/commands/pick.js
+++ Script/commands/pick.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pick",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "Shaon Ahmed",
   description: "Pinterest image search",
   commandCategory: "Media",
   usages: "pic <keyword> [amount]",
